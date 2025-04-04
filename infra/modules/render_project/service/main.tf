9520ded17ff6012ba8b2019ff3aeea4163fb7569
Eddie4k-code <edwardoc@buffalo.edu>
2025-04-03 22:15:40 -0400

    private service

--- infra/modules/render_project/service/main.tf
+++ infra/modules/render_project/service/main.tf
@@ -1,5 +1,7 @@
-resource "render_private_Service" "service" {
-    name = var.name
-    plan = var.plan
-    region = var.region
+resource "render_private_service" "service" {
+    for_each = {for s in var.services : s.name => s}
+    region   = each.value.region
+    name     = each.value.name
+    plan     = each.value.plan
+    runtime_source = each.value.runtime_source
 }
